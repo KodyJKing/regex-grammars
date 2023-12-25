4160d132b1513d45e4608fec83ed0f1a84d2abbc
KJK <kody.j.king@gmail.com>
2023-12-24 23:10:22 -0800

    Removed "forget" param from useLocal.

--- web/hooks/useLocal.ts
+++ web/hooks/useLocal.ts
@@ -1,10 +1,7 @@
 import { useEffect, useState } from "react"
 
-export function useLocal<T>( key: string, initialValue: T, forget: boolean ): [ T, ( value: T ) => void ] {
+export function useLocal<T>( key: string, initialValue: T ): [ T, ( value: T ) => void ] {
     const [ value, setValue ] = useState<T>( () => {
-        if ( forget )
-            return initialValue
-
         const localValue = localStorage.getItem( key )
         try {
             return localValue ? JSON.parse( localValue ) : initialValue
